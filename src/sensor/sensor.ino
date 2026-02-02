2f6644cb74db407182757704b82994d60a632be4
apempen <104975628+apempen@users.noreply.github.com>
2026-02-02 22:30:29 +0900

    M5StickCPlus.h を使ったLチカ

---
+++ src/sensor/sensor.ino
@@ -0,0 +1,21 @@
+#include <M5StickCPlus.h>
+
+void setup() {
+  M5.begin();
+
+  Serial.begin(115200);
+  delay(500);
+  Serial.print("M5StickC-plus Start\n");
+
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(10, HIGH); // turn the LED on (HIGH is the voltage level)
+  Serial.print("LED TURN ON\n");
+  delay(1000); // wait for a second
+  digitalWrite(10, LOW); // turn the LED off by making the voltage LOW
+  Serial.print("LED TURN OFF\n");
+  delay(1000); // wait for a second
+}
